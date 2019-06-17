4d0aeb8a045f683182163c60ea110c8fbf06ac1f
Murthy Kavali <murthykavali@users.noreply.github.com>
2019-06-17 12:10:49 +0530

    To test wdc
    WDC tested locally

---
+++ earthquakeWDC.js
@@ -0,0 +1,55 @@
+(function () {
+    var myConnector = tableau.makeConnector();
+
+myConnector.getSchema = function (schemaCallback) {
+	    var cols = [{
+	        id: "id",
+	        dataType: tableau.dataTypeEnum.string
+	    }, {
+	        id: "name",
+	        alias: "Name",
+	        dataType: tableau.dataTypeEnum.string
+	    }, {
+	        id: "age",
+	        alias: "Age",
+	        dataType: tableau.dataTypeEnum.int
+	    }];
+
+	    var tableSchema = {
+	        id: "earthquakeFeed",
+	        alias: "Earthquakes with magnitude greater than 4.5 in the last seven days",
+	        columns: cols
+	    };
+
+	    schemaCallback([tableSchema]);
+	};
+
+myConnector.getData = function(table, doneCallback) {
+	    $.getJSON("https://tab-test1.free.beeceptor.com/sample-data", function(resp) {
+	        var tableData = [];
+
+	        // Iterate over the JSON object
+	        for (var i = 0, len = resp.length; i < len; i++) {
+	            tableData.push({
+	                "id": resp[i].id,
+	                "name": resp[i].name,
+	                "age": resp[i].age
+	                
+	            });
+	        }
+
+	        table.appendRows(tableData);
+	        doneCallback();
+	    });
+	};
+
+    tableau.registerConnector(myConnector);
+
+    $(document).ready(function () {
+	    $("#submitButton").click(function () {
+	        tableau.connectionName = "USGS Earthquake Feed";
+	        tableau.submit();
+	    });
+	});
+
+})();
