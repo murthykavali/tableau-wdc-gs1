d034aa1047f9846d4248452a39f23a88ad841a7f
Azhar Uddin <akhan@gainsight.com>
2019-06-20 16:46:41 +0530

    Making changes to auth added token logic

--- earthquakeWDC.js
+++ earthquakeWDC.js
@@ -1,55 +1,77 @@
 (function () {
-    var myConnector = tableau.makeConnector();
 
-myConnector.getSchema = function (schemaCallback) {
-	    var cols = [{
-	        id: "id",
-	        dataType: tableau.dataTypeEnum.string
-	    }, {
-	        id: "name",
-	        alias: "Name",
-	        dataType: tableau.dataTypeEnum.string
-	    }, {
-	        id: "age",
-	        alias: "Age",
-	        dataType: tableau.dataTypeEnum.int
-	    }];
+	var a = new URLSearchParams(window.location.search);
+	var token = a.get("token");
+	debugger;
+	if (token) {
 
-	    var tableSchema = {
-	        id: "earthquakeFeed",
-	        alias: "Earthquakes with magnitude greater than 4.5 in the last seven days",
-	        columns: cols
-	    };
+		alert("Token Found");
+	
+		//Make call and dance. !!
+		var myConnector = tableau.makeConnector();
 
-	    schemaCallback([tableSchema]);
-	};
+		myConnector.getSchema = function (schemaCallback) {
+			var cols = [{
+				id: "id",
+				dataType: tableau.dataTypeEnum.string
+			}, {
+				id: "name",
+				alias: "Name",
+				dataType: tableau.dataTypeEnum.string
+			}, {
+				id: "age",
+				alias: "Age",
+				dataType: tableau.dataTypeEnum.int
+			}];
 
-myConnector.getData = function(table, doneCallback) {
-	    $.getJSON("https://tab-test1.free.beeceptor.com/sample-data", function(resp) {
-	        var tableData = [];
+			var tableSchema = {
+				id: "earthquakeFeed",
+				alias: "Earthquakes with magnitude greater than 4.5 in the last seven days",
+				columns: cols
+			};
 
-	        // Iterate over the JSON object
-	        for (var i = 0, len = resp.length; i < len; i++) {
-	            tableData.push({
-	                "id": resp[i].id,
-	                "name": resp[i].name,
-	                "age": resp[i].age
-	                
-	            });
-	        }
+			schemaCallback([tableSchema]);
+		};
 
-	        table.appendRows(tableData);
-	        doneCallback();
-	    });
-	};
+		myConnector.getData = function (table, doneCallback) {
+			$.getJSON("https://tab-test1.free.beeceptor.com/sample-data", function (resp) {
+				var tableData = [];
 
-    tableau.registerConnector(myConnector);
+				// Iterate over the JSON object
+				for (var i = 0, len = resp.length; i < len; i++) {
+					tableData.push({
+						"id": resp[i].id,
+						"name": resp[i].name,
+						"age": resp[i].age
 
-    $(document).ready(function () {
-	    $("#submitButton").click(function () {
-	        tableau.connectionName = "USGS Earthquake Feed";
-	        tableau.submit();
-	    });
-	});
+					});
+				}
+
+				table.appendRows(tableData);
+				doneCallback();
+			});
+		};
+
+
+		tableau.registerConnector(myConnector);
+
+	} else {
+		window.location.url = "https://abstract-gong360.develgs.com/v1/mob/signin"
+		// $(document).ready(function () {
+		// 	$("#submitButton").click(function () {
+		// 		tableau.connectionName = "USGS Earthquake Feed";
+		// 		tableau.submit();
+		// 	});
+		// });
+	}
+
+
+
+
+
+
+
+
+	
 
 })();
